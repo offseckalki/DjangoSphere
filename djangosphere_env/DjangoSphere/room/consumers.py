183cbed79e3c0e359f9080698479f04b7c7897e4
Drash Tyagi <tyagidrash21@gmail.com>
2024-01-17 01:59:15 +0530

    Added The Real-Time Delete Feautre

--- djangosphere_env/DjangoSphere/room/consumers.py
+++ djangosphere_env/DjangoSphere/room/consumers.py
@@ -1,3 +1,5 @@
+# consumers.py
+
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
@@ -26,27 +28,42 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         if 'message' in data:
-            message = data['message']
-            username = data['username']
-            room = data['room']
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
-            deletable = data.get('deletable', True)  # Default to True if not provided
-
-            await self.save_message(username, room, message, timestamp, deletable)
-
-            await self.channel_layer.group_send(
-                self.room_group_name,
-                {
-                    'type': 'chat.message',
-                    'message': message,
-                    'username': username,
-                    'room': room,
-                    'timestamp': timestamp,
-                    'deletable': deletable,
-                }
-            )
+            await self.handle_chat_message(data)
         elif 'delete' in data:
-            await self.delete_message(data['delete']['message_id'])
+            await self.handle_message_deletion(data)
+
+    async def handle_chat_message(self, data):
+        message = data['message']
+        username = data['username']
+        room = data['room']
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
+        deletable = data.get('deletable', True)  # Default to True if not provided
+
+        await self.save_message(username, room, message, timestamp, deletable)
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat.message',
+                'message': message,
+                'username': username,
+                'room': room,
+                'timestamp': timestamp,
+                'deletable': deletable,
+            }
+        )
+
+    async def handle_message_deletion(self, data):
+        await self.delete_message(data['delete']['message_id'])
+
+        # Broadcast a message indicating the deletion to all clients
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat.message_deleted',
+                'message_id': data['delete']['message_id'],
+            }
+        )
 
     async def chat_message(self, event):
         await self.send(text_data=json.dumps({
@@ -55,6 +72,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'room': event['room'],
             'timestamp': event['timestamp'],
             'deletable': event['deletable'],
+        }))
+
+    async def chat_message_deleted(self, event):
+        # Notify all clients that a message has been deleted
+        await self.send(text_data=json.dumps({
+            'message_deleted': event['message_id'],
         }))
 
     @sync_to_async
