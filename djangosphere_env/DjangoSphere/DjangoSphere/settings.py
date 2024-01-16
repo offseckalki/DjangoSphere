f77f4eaeb5be34a5918141cc01b05952f96a117f
Drash Tyagi <tyagidrash21@gmail.com>
2024-01-17 02:40:33 +0530

    Download that uploaded file, Feautre

--- djangosphere_env/DjangoSphere/DjangoSphere/settings.py
+++ djangosphere_env/DjangoSphere/DjangoSphere/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_REDIRECT_URL = '/rooms/'
 LOGIN_URL = '/login/'
