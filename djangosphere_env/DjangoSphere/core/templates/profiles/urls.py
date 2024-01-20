1cbf9699a157107bd78a17c6848ae5fdee2733e2
Drash Tyagi <tyagidrash21@gmail.com>
2024-01-20 21:39:10 +0530

    added Public Profile

--- djangosphere_env/DjangoSphere/core/templates/profiles/urls.py
+++ djangosphere_env/DjangoSphere/core/templates/profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from core import views  # Adjust the import path based on your actual structure
+from core.views import user_profile, search_users
 
 urlpatterns = [
-    path('profile/', views.user_profile, name='user_profile'),
+    path('search/', search_users, name='search_users'),  # Add this line for search functionality
+    path('<str:username>/', user_profile, name='user_profile'),
     # Add other URL patterns for your profiles app as needed
 ]
